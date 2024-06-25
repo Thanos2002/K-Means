196d95d2a35b8bbed002d9a4acd8c635f6ee5b54
Thanasis Akriotis <115248278+Thanos2002@users.noreply.github.com>
2024-06-25 20:52:10 +0300

    Add files via upload

---
+++ KMeans.java
@@ -0,0 +1,179 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
+
+public class KMeans {
+    private static final int M = 9;
+
+    static class Point {
+        double x;
+        double y;
+        int cluster;
+        Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Point> data = readCSV("points.csv");
+        
+        List<Point> centroids = initializeCentroids(data);
+
+        int t = 0; 
+
+        while (true) {
+            assignToClusters(data, centroids);
+ 
+            List<Point> newCentroids = calculateCentroids(data, M);  
+
+            if (centroidsEqual(centroids, newCentroids)) {
+                break; 
+            }
+            centroids = newCentroids;
+
+            t++;
+        }
+        System.out.println("The algorithm made : " + t + " iterations");
+        saveClusters(data, "output_points.csv");
+        saveCentroids(centroids, "output_centroids.csv");
+        System.out.println("Clustering error is : " + clusteringError(data,centroids));
+
+        //printCl(data);
+        //printCe(centroids);
+
+    }
+
+     private static List<Point> readCSV(String fileName) {
+        List<Point> data = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                String[] values = scanner.nextLine().split(",");
+                double x = Double.parseDouble(values[0]);
+                double y = Double.parseDouble(values[1]);
+                data.add(new Point(x, y));
+            }
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+
+      private static void saveClusters(List<Point> data, String fileName) {
+        try (PrintWriter writer = new PrintWriter(new File(fileName))) {
+            for (Point point : data) {
+                writer.println(point.x + "," + point.y);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveCentroids(List<Point> centroids, String fileName) {
+        try (PrintWriter writer = new PrintWriter(new File(fileName))) {
+            for (int i = 0; i < centroids.size(); i++) {
+                Point centroid = centroids.get(i);
+                writer.println(centroid.x + "," + centroid.y);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static List<Point> initializeCentroids(List<Point> data) {
+        List<Point> centroids = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < M; i++) {
+            int randomNum = random.nextInt(data.size());
+            centroids.add(new Point(data.get(randomNum).x, data.get(randomNum).y));
+        }
+
+        return centroids;
+    }
+
+    private static void assignToClusters(List<Point> data, List<Point> centroids) {
+        for (Point point : data) {
+            double minDistance = 10.0;
+            int cluster = -1;
+
+            for (int i = 0; i < M; i++) {
+                double distance = euclideanDistance(point, centroids.get(i));
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    cluster = i;
+                }
+            }
+
+            point.cluster = cluster;
+        }
+    }
+
+    private static List<Point> calculateCentroids(List<Point> data, int M) {
+        List<Point> centroids = new ArrayList<>();
+
+        for (int j = 0; j < M; j++) {
+            double sumX = 0;
+            double sumY = 0;
+            int size = 0;
+
+            for (Point point : data) {
+                if (point.cluster == j) {
+                    sumX += point.x;
+                    sumY += point.y;
+                    size++;
+                }
+            }
+            double cx = sumX / size;
+            double cy = sumY / size;
+            centroids.add(new Point(cx, cy));
+        }
+
+        return centroids;
+    }
+
+    private static double euclideanDistance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+
+    private static boolean centroidsEqual(List<Point> c1, List<Point> c2) {
+
+        for (int i = 0; i < c1.size(); i++) {
+            if (euclideanDistance(c1.get(i), c2.get(i)) > 0.001) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static double clusteringError(List<Point> data, List<Point> centroids){
+        double error = 0;
+    
+        for (int i = 0; i < data.size(); i++) {
+
+            double distance = euclideanDistance(data.get(i), centroids.get(data.get(i).cluster));
+            error += distance;
+        }
+
+        return error;
+    }
+
+    private static void printCl(List<Point> data) {
+        for (Point point : data) {
+            System.out.println("Point (" + point.x + ", " + point.y + ") belongs to Cluster " + (point.cluster+1));
+        }
+    }
+
+    private static void printCe(List<Point> centroids) {
+        for (int i = 0; i < centroids.size(); i++) {
+            Point centroid = centroids.get(i);
+            System.out.println("Centroid " + (i+1) + ": (" + centroid.x + ", " + centroid.y + ")");
+        }
+    }
+}
---
+++ RandomPointsGenerator.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RandomPointsGenerator {
+
+    public static void main(String[] args) {
+        List<double[]> points = generateRandomPoints();
+
+        /*
+        for (int i = 0; i < points.size(); i++) {
+            double[] point = points.get(i);
+            System.out.println("Point " + (i + 1) + ": (" + point[0] + ", " + point[1] + ")");
+        }
+        */
+
+       try (FileWriter writer = new FileWriter("points.csv")) {
+            for (double[] point : points) {
+                writer.write(point[0] + "," + point[1] + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<double[]> generateRandomPoints() {
+        List<double[]> points = new ArrayList<>();
+
+        points.addAll(generatePointsInSquare(150, 0.8, 1.2, 0.8, 1.2));
+        points.addAll(generatePointsInSquare(150, 0, 0.5, 0, 0.5));
+        points.addAll(generatePointsInSquare(150, 1.5, 2, 0, 0.5));
+        points.addAll(generatePointsInSquare(150, 0, 0.5, 1.5, 2));
+        points.addAll(generatePointsInSquare(150, 1.5, 2, 1.5, 2));
+        points.addAll(generatePointsInSquare(75, 0, 0.4, 0.8, 1.2));
+        points.addAll(generatePointsInSquare(75, 1.6, 2, 0.8, 1.2));
+        points.addAll(generatePointsInSquare(75, 0.8, 1.2, 0.3, 0.7));
+        points.addAll(generatePointsInSquare(75, 0.8, 1.2, 1.3, 1.7));
+        points.addAll(generatePointsInSquare(150, 0, 2, 0, 2));
+
+        return points;
+    }
+
+    private static List<double[]> generatePointsInSquare(int count, double minX, double maxX, double minY, double maxY) {
+        List<double[]> points = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++) {
+            double x = minX + (maxX - minX) * random.nextDouble();
+            double y = minY + (maxY - minY) * random.nextDouble();
+            double[] point = new double[2];
+            point[0] = x;
+            point[1] = y;
+            points.add(point);
+        }
+
+        return points;
+    }
+}
